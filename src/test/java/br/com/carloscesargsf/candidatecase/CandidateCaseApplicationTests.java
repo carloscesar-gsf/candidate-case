9c4b4edea0efa56a88b204bdb1d20062e778ef25
Carlos César Gomes dos Santos Filho <carloscesar.gsf@gmail.com>
2020-07-09 21:12:38 -0300

    Added test profile and removed unused test case.

--- src/test/java/br/com/carloscesargsf/candidatecase/CandidateCaseApplicationTests.java
+++ src/test/java/br/com/carloscesargsf/candidatecase/CandidateCaseApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.carloscesargsf.candidatecase;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CandidateCaseApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
